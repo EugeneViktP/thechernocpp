bb7faf9e76683b564afac7a86ec84e894af184f0
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-03 20:59:49 +0300

    Lesson 22

--- Lesson_22/Main.cpp
+++ Lesson_22/Main.cpp
@@ -4,7 +4,7 @@
 struct Entity
 {
     static int x, y;
-    void Print()
+    static void Print()
     {
         std::cout << x << ", " << y << std::endl;
     }
@@ -15,18 +15,18 @@ int Entity::y;
 
 int main()
 {
-    Entity e;
+    //Entity e;
     Entity::x = 2;
     Entity::y = 3;
 
 
-    Entity e1;
+    /*Entity e1;
 
-    Entity::x = 5;
-    Entity::y = 8;
+    e1.x = 5;
+    e1.y = 8;
 
-    e.Print();
-    e1.Print();
+    Entity::Print();*/
+    Entity::Print();
     
     std::cin.get();
 }
---
+++ Pointer_Ref/Pointer_Ref.cpp
@@ -0,0 +1,18 @@
+﻿// Pointer_Ref.cpp 
+//
+
+#include <iostream>
+
+int main()
+{
+    int x = 10;
+    std::cout << "Initial Value: " << x << std::endl;
+    int* ptr = &x;
+    *ptr = 7;
+    std::cout << "Current value (ptr): " << x << std::endl;
+
+    int& ref = x;
+    ref = 9;
+    std::cout << "Current value (ref): " << x << std::endl;
+    std::cin.get();
+}
