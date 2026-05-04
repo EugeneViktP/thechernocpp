0a290425b2f6292bdccaf7064c4faba9ce67a0c4
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-04 20:33:07 +0300

    updated Log and added Enum

---
+++ Enums_23/Enums_23.cpp
@@ -0,0 +1,23 @@
+﻿// Enums_23.cpp :
+//
+
+#include <iostream>
+
+enum Example : char
+{
+    A, B, C
+};
+
+int a = 0;
+int b = 1;
+int c = 2;
+
+
+int main()
+{
+    Example value = B;
+    
+    std::cin.get();
+}
+
+
--- Logging/Logging.cpp
+++ Logging/Logging.cpp
@@ -8,13 +8,16 @@
 class Log
 {
 public:
-    const int LogLevelError = 0;
-    const int LogLevelWarning = 1;
-    const int LogLevelInfo = 2;
+
+    enum Level
+    {
+        LevelError = 0, LevelWarning, LevelInfo
+    };
+   
 private:
-    int m_LogLevel = LogLevelInfo;
+    Level m_LogLevel = LevelInfo;
 public:
-    void SetLevel(int level)
+    void SetLevel(Level level)
     {
         m_LogLevel = level;
 
@@ -22,19 +25,19 @@ public:
 
     void Error(const char* message)
     {
-        if (m_LogLevel >= LogLevelError)
+        if (m_LogLevel >= LevelError)
         std::cout << "[ERROR]: " << message << std::endl;
     }
 
     void Warn(const char* message)
     {
-        if (m_LogLevel >= LogLevelWarning)
+        if (m_LogLevel >= LevelWarning)
         std::cout << "[WARNING]: " << message << std::endl;
     }
 
     void Info(const char* message)
     {
-        if (m_LogLevel >= LogLevelInfo)
+        if (m_LogLevel >= LevelInfo)
         std::cout << "[INFO]: " << message << std::endl;
     }
 
@@ -44,7 +47,7 @@ public:
 int main()
 {
     Log log;
-    //log.SetLevel(log.LogLevelWarning);
+    log.SetLevel(Log::LevelWarning);
     log.Warn("Hello");
     log.Error("Hello");
     log.Info("Hello");
