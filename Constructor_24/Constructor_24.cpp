2afd666efe1430136f6a9814b6d4c7a3ae7342a7
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-05 21:08:29 +0300

    Lesson 24 Constructor

---
+++ Constructor_24/Constructor_24.cpp
@@ -0,0 +1,36 @@
+﻿// Constructor_24.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+
+class Entity
+{
+public:
+    float X, Y;
+
+
+    Entity()
+    {
+
+    }
+
+    Entity(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    void Print()
+    {
+        std::cout << X << ", " << Y << std::endl;
+    }
+};
+
+int main()
+{
+    Entity e(10.0f, 5.0f);
+    e.Print();
+
+    std::cin.get();
+}
