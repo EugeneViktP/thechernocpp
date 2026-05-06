3fbff60f61ddcea86717c478cdfd8a68badd14bb
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-06 19:51:34 +0300

    Lesson 25 Destructor

---
+++ Destructor_25/Destructor_25.cpp
@@ -0,0 +1,49 @@
+﻿// Constructor_24.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+
+class Entity
+{
+public:
+    float X, Y;
+
+
+    Entity()
+    {
+        X = 0.0f;
+        Y = 0.0f;
+
+        std::cout << "Created Entity!" << std::endl;
+    }
+
+    Entity(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    ~Entity()
+    {
+        std::cout << "Destroyed Entity!" << std::endl;
+    }
+
+    void Print()
+    {
+        std::cout << X << ", " << Y << std::endl;
+    }
+};
+
+void Function()
+{
+    Entity e;
+    e.Print();
+}
+
+int main()
+{
+    Function();
+
+    std::cin.get();
+}
