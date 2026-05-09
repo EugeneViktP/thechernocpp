ce114a09fe08f53f23ea4e56641a5de6c4f6a5fa
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-09 18:39:49 +0300

    Inheritance lesson 26

---
+++ Inheritance_26/Inheritance_26.cpp
@@ -0,0 +1,48 @@
+﻿// Inheritance_26.cpp 
+//
+
+#include <iostream>
+
+class Entity
+{
+public:
+    float X, Y;
+
+    void Move(float xa, float ya)
+    {
+        X += xa;
+        Y += ya;
+    }
+};
+
+class Player : public Entity
+{
+public:
+    
+    const char* name;
+   
+
+    void printName()
+    {
+
+    }
+};
+
+int main()
+{
+    
+    std::cout << sizeof(Entity) << std::endl;
+    std::cout << sizeof(Player) << std::endl;
+    
+    
+    Player player;
+    player.printName();
+    player.Move(5, 5);
+    player.X = 2;
+    player.Y = 3;
+    std::cout << sizeof(player.name) << std::endl;
+
+    
+    std::cin.get();
+}
+
