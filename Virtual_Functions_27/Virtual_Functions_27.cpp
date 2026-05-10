14b0d415b540dcc4363816798ce1b9331c35420e
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-10 10:21:27 +0300

    Virtual functions lesson 27

---
+++ Virtual_Functions_27/Virtual_Functions_27.cpp
@@ -0,0 +1,46 @@
+﻿// Virtual_Functions_27.cpp :
+//
+
+#include <iostream>
+#include <string>
+
+
+class Entity
+{
+public:
+    virtual std::string getName() { return "Entity"; }
+};
+
+class Player : public Entity
+{
+private:
+    std::string m_Name;
+
+public:
+    Player(const std::string& name)
+        : m_Name(name) {}
+
+    std::string getName() override { return m_Name; }
+
+};
+
+void printName(Entity* entity)
+{
+    std::cout << entity->getName() << std::endl;
+}
+
+int main()
+{
+    Entity* e = new Entity();
+    printName(e);
+
+    Player* p = new Player("Cherno");
+    printName(p);
+
+    /*Entity* entity = p;
+    std::cout << entity->getName() << std::endl;*/
+
+    std::cin.get();
+}
+
+
