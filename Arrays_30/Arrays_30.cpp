2dbb128f05563e6141c561cf1cf43d6eda6c246b
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-11 17:03:55 +0300

    Arrays lesson 30

---
+++ Arrays_30/Arrays_30.cpp
@@ -0,0 +1,30 @@
+﻿// Arrays_30.cpp :
+//
+
+#include <iostream>
+
+int main()
+{
+    int example[5];
+
+    int* ptr = example;
+
+    for (int i = 0; i < 5; i++)
+    {
+        example[i] = 2;
+    }
+
+    example[2] = 5;
+
+    *(ptr + 2) = 6;
+    /*example[0] = 2;
+    example[4] = 4;
+
+    std::cout << example[0] << std::endl;
+    std::cout << example << std::endl;*/
+
+
+    
+    std::cin.get();
+}
+
