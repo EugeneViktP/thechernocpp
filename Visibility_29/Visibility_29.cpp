57962ebbddde50e1460c27e290efd882b1a6dbe2
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-11 15:59:09 +0300

    Visibility lesson 29

---
+++ Visibility_29/Visibility_29.cpp
@@ -0,0 +1,40 @@
+﻿// Visibility_29.cpp : 
+//
+
+#include <iostream>
+#include <string>
+
+class Entity
+{
+protected:
+    int X, Y;
+    void print()
+    {
+        std::cout << "Hello" << std::endl;
+    }
+public:
+    Entity()
+    {
+        X = 0;
+    }
+};
+
+class Player : public Entity
+{
+public:
+    Player()
+    {
+        X = 2;
+        print();
+    }
+};
+
+int main()
+{
+    Entity e;
+    //e.X = 2; This doesn't work due to visibility issues
+    //e.print();
+
+    std::cin.get();
+}
+
