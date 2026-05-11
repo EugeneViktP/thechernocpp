dec0b6bbe86bc50c54fcb826f44187a537e80b02
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-11 19:18:51 +0300

    Updated arrays lesson 30

--- Arrays_30/Arrays_30.cpp
+++ Arrays_30/Arrays_30.cpp
@@ -3,25 +3,37 @@
 
 #include <iostream>
 
-int main()
+class Entity
 {
-    int example[5];
+public:
 
-    int* ptr = example;
+    static const int size = 5;
+    
+    int example[size];
 
+    Entity()
+    {
+        int a[5];
+        int count = sizeof(a) / sizeof(int);
+        
+        for (int i = 0; i < size; i++)
+        {
+            example[i] = 2;
+        }
+    }
+};
+
+
+int main()
+{    
+    Entity e;
+    
+    /*int* another = new int[5];
     for (int i = 0; i < 5; i++)
     {
-        example[i] = 2;
+        another[i] = 2;
     }
-
-    example[2] = 5;
-
-    *(ptr + 2) = 6;
-    /*example[0] = 2;
-    example[4] = 4;
-
-    std::cout << example[0] << std::endl;
-    std::cout << example << std::endl;*/
+    delete[] another;*/
 
 
     
