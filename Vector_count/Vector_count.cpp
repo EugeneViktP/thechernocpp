665f5fcb2d19e2c1ca2c0a35f675514c2cca6502
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-11 20:19:48 +0300

    Vector count t_1

---
+++ Vector_count/Vector_count.cpp
@@ -0,0 +1,23 @@
+﻿// Vector_count.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int countData(const std::vector<bool>& dataset)
+{
+    int result = count(dataset.begin(), dataset.end(), true);
+    return result;
+}
+
+
+
+int main()
+{
+    std::cout << "Count quantity: ";
+    std::cout << countData({ true, false, true, true, false, true }) << std::endl;
+    std::cin.get();
+}
+
+
