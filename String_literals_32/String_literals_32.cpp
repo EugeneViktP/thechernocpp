308467d84b58d968cc5ead31577f8bc6a0d5cfcc
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-13 21:11:58 +0300

    String literals lesson 32

---
+++ String_literals_32/String_literals_32.cpp
@@ -0,0 +1,22 @@
+﻿// String_literals_32.cpp : 
+//
+
+#include <iostream>
+#include <string>
+
+#include <stdlib.h>
+
+int main()
+{
+    using namespace std::string_literals;
+    
+    std::string name0 = "Cherno"s + "hello";
+    
+    
+    const char* name = "Cherno";
+    const wchar_t* name2 = L"Cherno";
+    const char16_t* name3 = u"Cherno";
+    std::cout << name << std::endl;
+    
+    std::cin.get();
+}
