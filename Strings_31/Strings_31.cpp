0ff9a47a0dba6c38d9272cb31057e00fd8327317
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-13 18:17:40 +0300

    Strings lesson 31

---
+++ Strings_31/Strings_31.cpp
@@ -0,0 +1,22 @@
+﻿// Strings_31.cpp : 
+//
+
+#include <iostream>
+#include <string>
+
+
+void PrintString(const std::string& string)
+{
+    std::cout << string << std::endl;
+}
+
+int main()
+{
+    std::string name = std::string("Cherno") + " hello!";
+    PrintString(name);
+    bool contains = name.find("no") != std::string::npos;
+    std::cout << name << std::endl;
+    
+    std::cin.get();
+}
+
