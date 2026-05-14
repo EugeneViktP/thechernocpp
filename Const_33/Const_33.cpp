bb86952b6dcb4df540c1f6a53bddbc18c892bd86
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-14 21:37:08 +0300

    Sorting

--- Const_33/Const_33.cpp
+++ Const_33/Const_33.cpp
@@ -3,12 +3,36 @@
 
 #include <iostream>
 
+
+class Entity
+{
+private:
+    int m_X, m_Y;
+    mutable int var;
+public:
+    int GetX() const
+    {
+        var = 2;
+        return m_X;
+    }
+
+    void SetX(int x)
+    {
+        m_X = x;
+    }
+};
+
+void PrintEntity(const Entity& e)
+{
+    std::cout << e.GetX() << std::endl;
+}
+
 int main()
 {
     const int MAX_AGE = 90;
 
 
-    int* a = new int;
+    const int* const a = new int;
 
     *a = 2;
 
---
+++ Sort_people/Sort_people.cpp
@@ -0,0 +1,37 @@
+﻿// Sort_people.cpp : 
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    using namespace std::string_literals;
+    int count;
+    std::cin >> count;
+    std::vector<std::pair<int, std::string>> people{};
+    for (int i = 0; i < count; i++)
+    {
+        std::string name;
+        int score;
+        std::cin >> name >> score;
+        people.push_back({ score, name });
+    }
+
+    std::sort(people.begin(), people.end());
+    std::reverse(people.begin(), people.end());
+
+    for (const auto& element : people)
+    {
+        std::cout << element.second << " "s << std::endl;
+    }
+
+    
+    
+    
+    
+    std::cin.get();
+}
+
