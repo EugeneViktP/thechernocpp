364f3266eab5f74ec6509d267ab7689bcfba34f3
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-14 18:44:53 +0300

    Const lesson 33

---
+++ Const_33/Const_33.cpp
@@ -0,0 +1,23 @@
+﻿// Const_33.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    const int MAX_AGE = 90;
+
+
+    int* a = new int;
+
+    *a = 2;
+
+    a = (int*) & MAX_AGE;
+
+    std::cout << *a << std::endl;
+
+    
+    std::cin.get();
+}
+
+
