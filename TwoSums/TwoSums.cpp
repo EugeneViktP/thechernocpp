295988be9fa0dea7c4f70ab9900a00380ccbec50
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-16 08:33:44 +0300

    Two Sums for sorted array

---
+++ TwoSums/TwoSums.cpp
@@ -0,0 +1,43 @@
+﻿// TwoSums.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        int sum = 0;
+
+        while (l < r)
+        {
+            sum = nums[l] + nums[r];
+            if (sum == target)
+            {
+                std::cout << l << r << std::endl;
+                return { l, r };
+            }
+            else if (sum > target)
+            {
+                r--;
+            }
+            else {
+                l++;
+            }
+        }
+
+    }
+};
+
+int main()
+{
+    Solution sol;
+    std::vector<int> vec = { 2, 7, 10, 15 };
+
+    sol.twoSum(vec, 9);
+    
+    std::cin.get();
+}
+
