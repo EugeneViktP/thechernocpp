01f521ad6300fd2c741b081edd06c849b96dbe16
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-17 12:54:15 +0300

    Palindrome for single words

---
+++ Palindrome/Palindrome.cpp
@@ -0,0 +1,33 @@
+﻿// Palindrome.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+class Solution {
+public:
+    bool isPalindrome(std::string s) {
+        int l = 0;
+        size_t r = s.length() - 1;
+        while (l < r)
+        {
+            if (s[l] != s[r])
+                {
+                    return false; 
+                }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    std::cout << sol.isPalindrome("anamana") << std::endl;
+
+    std::cin.get();
+}
+
+
