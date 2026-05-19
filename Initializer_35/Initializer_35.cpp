6de0fe9bf1a5108427289da896a9a7e3e44fd5fa
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-19 21:06:06 +0300

    Initializer lesson 35

--- Initializer_35/Initializer_35.cpp
+++ Initializer_35/Initializer_35.cpp
@@ -4,15 +4,33 @@
 #include <iostream>
 #include <string>
 
+
+class Example
+{
+public:
+    Example()
+    {
+        std::cout << "Created Entity" << std::endl;
+    }
+
+    Example(int x)
+    {
+        std::cout << "Created Entity with " << x << "!" << std::endl;
+    }
+};
+
+
+
 class Entity
 {
 private:
     std::string m_Name;
+    Example m_Example;
 public:
     Entity()
-        : m_Name("Unknown")
+        : m_Example(8)
     {
-        
+        m_Name = std::string("Unknown");
     }
     Entity(const std::string& name)
         : m_Name(name)
@@ -25,10 +43,7 @@ public:
 int main()
 {
     Entity e0;
-    std::cout << e0.GetName() << std::endl;
-
-    Entity e1("Cherno");
-    std::cout << e1.GetName() << std::endl;
+    
     std::cin.get();
 }
 
