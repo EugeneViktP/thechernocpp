774fca7ec551a13fff83d852445936676c6029de
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-20 20:58:26 +0300

    Ternary 36

---
+++ Ternary_36/Ternary_36.cpp
@@ -0,0 +1,25 @@
+﻿// Ternary_36.cpp : 
+//
+
+#include <iostream>
+
+static int s_Level = 8;
+static int s_Speed = 2;
+
+int main()
+{
+    if (s_Level > 5)
+        s_Speed = 10;
+    else
+        s_Speed = 5;
+
+    /*s_Speed = s_Level > 5 ? 10 : 5;
+
+    std::string rank = s_Level > 10 ? "Master" : "Beginner ";*/
+
+    s_Speed = s_Level > 5 ? s_Level > 10 ? 15 : 10 : 5;
+    std::cout << s_Speed << std::endl;
+    
+    
+    std::cin.get();
+}
