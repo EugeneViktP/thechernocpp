4047ae4233911655ed63958e1dca51dc7aeb11a1
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-20 21:21:25 +0300

    Sorting out

---
+++ ObjectCreation_37/ObjectCreation_37.cpp
@@ -0,0 +1,27 @@
+﻿// ObjectCreation_37.cpp : 
+//
+
+#include <iostream>
+#include <string>
+
+using String = std::string;
+
+class Entity
+{
+private:
+    String m_Name;
+public:
+    Entity() : m_Name("Unknown") {}
+    Entity(const String& name) : m_Name(name) {}
+
+    const String& GetName() const { return m_Name; }
+};
+
+int main()
+{
+    Entity entity;
+    std::cout << entity.GetName() << std::endl;
+    
+    std::cin.get();
+}
+
