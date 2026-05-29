2e9927efbe2a2fc9c37348a1ceb3293e68e9ee10
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-29 21:05:11 +0300

    Smart pointers 43

--- Lessons/SmartPointers/SmartPointers.cpp
+++ Lessons/SmartPointers/SmartPointers.cpp
@@ -25,8 +25,13 @@ public:
 int main()
 {
     {
-        std::unique_ptr<Entity> entity = std::make_unique<Entity>();
-        entity->Print();
+        std::shared_ptr<Entity> e0;
+        {
+            /*std::unique_ptr<Entity> entity = std::make_unique<Entity>();*/
+
+            std::shared_ptr<Entity> sharedEntity = std::make_shared<Entity>();
+            e0 = sharedEntity;
+        }
     }
     
     std::cin.get();
