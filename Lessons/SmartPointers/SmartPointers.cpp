fc9e2c2057f4ab08207f0e522e585f3fcac7b782
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-29 19:27:45 +0300

    Smart pointers 43

---
+++ Lessons/SmartPointers/SmartPointers.cpp
@@ -0,0 +1,34 @@
+﻿// SmartPointers.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <memory>
+
+
+class Entity
+{
+public:
+    Entity()
+    {
+        std::cout << "Created Entity!" << std::endl;
+    }
+
+    ~Entity()
+    {
+        std::cout << "Destroyed Entity!" << std::endl;
+    }
+
+    void Print() {}
+};
+
+int main()
+{
+    {
+        std::unique_ptr<Entity> entity = std::make_unique<Entity>();
+        entity->Print();
+    }
+    
+    std::cin.get();
+}
+
