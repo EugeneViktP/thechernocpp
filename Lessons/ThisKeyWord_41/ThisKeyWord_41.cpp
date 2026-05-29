8e4b54e7633c652af1c22e11b3ccae42db81f2d3
EugeneViktP <popoff.zhenya@yandex.ru>
2026-05-29 17:12:54 +0300

    ThisKeyWord lesson 41

---
+++ Lessons/ThisKeyWord_41/ThisKeyWord_41.cpp
@@ -0,0 +1,36 @@
+﻿// ThisKeyWord_41.cpp : 
+//
+
+#include <iostream>
+
+//void PrintEntity(Entity* e);
+
+class Entity
+{
+public:
+    int x, y;
+
+    Entity(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+        PrintEntity();
+    }
+
+    int GetX() const
+    {
+        const Entity* e = this;
+
+        return x;
+    }
+};
+
+void PrintEntity(Entity* e)
+{
+    //
+}
+
+int main()
+{
+    std::cin.get();
+}
