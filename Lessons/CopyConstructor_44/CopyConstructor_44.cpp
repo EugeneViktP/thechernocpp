6f10fb906731f6cfa04fbfa430c5884c72e6fa34
EugeneViktP <popoff.zhenya@yandex.ru>
2026-06-11 19:13:33 +0300

    Copying Constructor lesson 44

---
+++ Lessons/CopyConstructor_44/CopyConstructor_44.cpp
@@ -0,0 +1,24 @@
+﻿// CopyConstructor_44.cpp :
+//
+
+#include <iostream>
+#include <string>
+
+
+
+
+struct Vector2
+{
+    float x, y;
+};
+
+int main()
+{
+    
+    Vector2* a = new Vector2();
+    Vector2 b = a;
+    b.x = 5;
+
+
+    std::cin.get();
+}
