664c197ec816dac0109dc72409c8571ce13f556f
EugeneViktP <popoff.zhenya@yandex.ru>
2026-06-12 14:18:46 +0300

    Arrow operator lesson 45 byte serialization

--- Lessons/ArrowOperator_45/ArrowOperator_45.cpp
+++ Lessons/ArrowOperator_45/ArrowOperator_45.cpp
@@ -1,42 +1,18 @@
-﻿// ArrowOperator_45.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+﻿// ArrowOperator_45.cpp : 
 //
 
 #include <iostream>
 
-class Entity
+
+struct Vector3
 {
-public:
-    void Print() const { std::cout << "Hello!" << std::endl; }
-};
-
-class ScopedPtr
-{
-private:
-    Entity* m_Obj;
-public:
-    ScopedPtr(Entity* entity)
-        : m_Obj(entity)
-    {
-
-    }
-
-    ~ScopedPtr()
-    {
-        delete m_Obj;
-    }
-
-    Entity* operator->()
-    {
-        return m_Obj;
-    }
-
+    float x, y, z;
 };
 
 int main()
 {
-    ScopedPtr entity = new Entity;
-
-    entity->Print();
+    int offset = (int)&((Vector3*)nullptr)->z;
+    std::cout << offset << std::endl;
 
     std::cin.get();
 }
