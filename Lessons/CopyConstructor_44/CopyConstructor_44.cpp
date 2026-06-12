ef8b3e894f14801d99e742803bd31eda8393f344
EugeneViktP <popoff.zhenya@yandex.ru>
2026-06-12 10:10:53 +0300

    Copying Constructor lesson 44

--- Lessons/CopyConstructor_44/CopyConstructor_44.cpp
+++ Lessons/CopyConstructor_44/CopyConstructor_44.cpp
@@ -6,18 +6,66 @@
 
 
 
+class String
+{
+private:
+    char* m_Buffer;
+    unsigned int m_Size;
+public:
+    String(const char* string)
+    {
+        m_Size = strlen(string);
+        m_Buffer = new char[m_Size + 1];
+        memcpy(m_Buffer, string, m_Size);
+        m_Buffer[m_Size] = 0;
+    }
+
+    String(const String& other)
+        :m_Size(other.m_Size)
+    {
+        m_Buffer = new char[m_Size + 1];
+        memcpy(m_Buffer, other.m_Buffer, m_Size + 1);
+    }
+
+
+    ~String()
+    {
+        delete[] m_Buffer;
+    }
+
+    char& operator[](unsigned int index)
+    {
+        return m_Buffer[index];
+    }
+
+    friend std::ostream& operator<<(std::ostream& stream, const String& string);
+};
+
+std::ostream& operator<<(std::ostream& stream, const String& string)
+{
+    stream << string.m_Buffer;
+    return stream;
+}
+
 
 struct Vector2
 {
     float x, y;
 };
 
+
+
 int main()
 {
     
-    Vector2* a = new Vector2();
-    Vector2 b = a;
-    b.x = 5;
+    String string = "Cherno";
+    String second = string;
+
+    second[2] = 'a';
+
+    std::cout << string << std::endl;
+    std::cout << second << std::endl;
+
 
 
     std::cin.get();
